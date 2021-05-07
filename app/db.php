935af3435f0fd5de94c2134469ce81d8d423b56d
GGriddy <loic3.vdb@gmail.com>
2021-05-07 19:28:04 +0200

    main commit

--- app/db.php
+++ app/db.php
@@ -5,7 +5,7 @@ $db = null;
 try {
     $config = include($_SERVER['DOCUMENT_ROOT'].'/php_simple/config.php');
 
-    $db = new PDO("mysql:host=" . $config['database_host'] . ";dbname=" . $config['database_database'] . ";port=" . $config['database_port'], $config['database_user'], $config['database_password']);
+    $db = new PDO("mysql:host=" . $config['localhost'] . ";dbname=" . $config['blog_php'] . ";port=" . $config['3306'], $config['root'], $config['root']);
     // set the PDO error mode to exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(Exception $e) {
