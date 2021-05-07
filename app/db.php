5d1fc583f4f2760818f0c5500bb7666444b8975b
GGriddy <loic3.vdb@gmail.com>
2021-05-07 20:50:43 +0200

    phpmyadmin config

--- app/db.php
+++ app/db.php
@@ -5,7 +5,7 @@ $db = null;
 try {
     $config = include($_SERVER['DOCUMENT_ROOT'].'/config.php');
 
-    $db = new PDO("mysql:host=" . $config['localhost'] . ";dbname=" . $config['blog_php'] . ";port=" . $config['3306'], $config['root'], $config['root']);
+    $db = new PDO("mysql:host=" . $config['192.168.1.61'] . ";dbname=" . $config['mysql'] . ";port=" . $config['3306'], $config['root'], $config['QuickTime++8905']);
     // set the PDO error mode to exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(Exception $e) {
--- components/navigation.php
+++ components/navigation.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
         <div class="container-fluid">
             <a class="navbar-brand" href="/">
-                <img src="/blog/resources/images/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-top">
+                <img src="/resources/images/bootstrap-4.svg" alt="" width="30" height="24" class="d-inline-block align-top">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
